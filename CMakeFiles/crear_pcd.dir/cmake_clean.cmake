60f1096bdc09fde1423b01eea277d258b5351d28
Kath17 <kathy.uc.17@gmail.com>
2018-12-19 12:22:13 -0500

    carpetas

---
+++ CMakeFiles/crear_pcd.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/crear_pcd.dir/crear_pcd.cpp.o"
+  "crear_pcd.pdb"
+  "crear_pcd"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/crear_pcd.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
